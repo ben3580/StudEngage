5239100c609742a354b309144e41171faf23dfdb
Ben <60242695+ben3580@users.noreply.github.com>
2022-04-18 17:02:09 -0700

    Update MainActivity.java

--- app/src/main/java/com/example/iclikerapp/MainActivity.java
+++ app/src/main/java/com/example/iclikerapp/MainActivity.java
@@ -1,8 +1,6 @@
+import androidx.appcompat.app.AppCompatActivity;
 
-
-//import androidx.appcompat.app.AppCompatActivity;
-
-//import android.os.Bundle;
+import android.os.Bundle;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -10,53 +8,58 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class MainActivity{ //extends AppCompatActivity {
-//
-//    private static String ip =
-//    private static String port =
-//    private static String Classs =
-//    private static String database =
-    private static String sererUser = "studengage";
-    private static String serverPass = "WSUpullman123*";
-    private static String serverURL = "jdbc:sqlserver://studengage.database.windows.net:1433;database=StudEngage;user=studengage@studengage;password=WSUpullman123*;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+// public class MainActivity{ //extends AppCompatActivity {
+// //
+// //    private static String ip =
+// //    private static String port =
+// //    private static String Classs =
+// //    private static String database =
+//     private static String sererUser = "studengage";
+//     private static String serverPass = "WSUpullman123*";
+//     private static String serverURL = "jdbc:sqlserver://studengage.database.windows.net:1433;database=StudEngage;user=studengage@studengage;password=WSUpullman123*;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
 
 
-    public static void main(String[] args) {
+//     public static void main(String[] args) {
 
-        Connection con = null;
+//         Connection con = null;
 
-        String serverUser = "studengage";
-        String serverPass = "WSUpullman123*";
-        ResultSet rs = null;
+//         String serverUser = "studengage";
+//         String serverPass = "WSUpullman123*";
+//         ResultSet rs = null;
 
-        try {
-            con = DriverManager.getConnection(serverURL);
-            Statement statement = con.createStatement();
-            String qry = "SELECT stud_username FROM tblStudentUser WHERE stud_name = Sarah Harrison";
+//         try {
+//             con = DriverManager.getConnection(serverURL);
+//             Statement statement = con.createStatement();
+//             String qry = "SELECT stud_username FROM tblStudentUser WHERE stud_name = Sarah Harrison";
 
-            rs = statement.executeQuery(qry);
+//             rs = statement.executeQuery(qry);
 
-            while(rs.next()){
+//             while(rs.next()){
 
-                System.out.println(rs.getString(2));
+//                 System.out.println(rs.getString(2));
+//             }
+
+//         } catch (SQLException throwables) {
+//             throwables.printStackTrace();
+//         }
+//}}
+        
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Home.class);
+                startActivity(intent);
+                finish();
             }
-
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-
-
-
-
+        }, 2500);
 
     }
-
-
-
-
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//    }
-}
+} 
