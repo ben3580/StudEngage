555c3378644971e75ebb6ff930bfb2dcbb9ef152
tang-chris <christopher.tang@wsu.edu>
2022-04-21 16:21:15 -0700

    Update Communication.java

--- app/src/main/java/com/example/iclikerapp/Server/Communication.java
+++ app/src/main/java/com/example/iclikerapp/Server/Communication.java
@@ -1,5 +1,30 @@
 package com.example.iclikerapp.Server;
 
+import android.os.StrictMode;
+import android.util.Log;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Communication {
     
+    public Connection connectionclass(){
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection connection = null;
+        String ConnectionURL = null;
+        try{
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            ConnectionURL = "jdbc:jtds:sqlserver://studengage.database.windows.net:1433;DatabaseName=StudEngage;user=studengage@studengage;password=WSUpullman123;encrypt=true;trustServerCertificate=false;hostNameInCertificate=.database.windows.net;loginTimeout=30;ssl=request";
+            connection = DriverManager.getConnection(ConnectionURL);
+        } catch (ClassNotFoundException e) {
+            Log.e("error here 1 : ", e.getMessage());
+        } catch (SQLException se){
+            Log.e("error here 2 : ", se.getMessage());
+        } catch (Exception e){
+            Log.e("error here 3 : ", e.getMessage());
+        }
+        return connection;
+    }
 }
